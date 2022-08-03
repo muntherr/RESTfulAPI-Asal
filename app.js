1c23557e2f36743cd9f205d8d0107d4a625276b2
Muntherr Anati <mumtherranati@yahoo.com>
2022-08-03 15:15:38 +0300

    Handling DELETE Request

--- app.js
+++ app.js
@@ -26,25 +26,47 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// WE need to define a route like we done in http before
+// CRUD Operations:
+// POST --> Create
+// GET  --> Read
+// PUT  --> Update
+// PATCH-->
+// DELETE --> DELETE
 
+// Handling DELETEING  requests.
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'faild',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    // 204 --> There's no data, the data is deleted and return null to the client
+    status: 'success',
+    data: null,
+  });
+}); // We need to update the data
 
 app.patch('/api/v1/tours/:id', (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'faild',
-        message: 'Invalid ID',
-      });
-    }
-  
-    res.status(202).json({
-      status: 'success',
-      data: {
-        tour: '<Updated tour here..>',
-      },
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'faild',
+      message: 'Invalid ID',
     });
-      
-  }); // We need to update the data
-  
+  }
+
+  res.status(202).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here..>',
+    },
+  });
+}); // We need to update the data
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -99,28 +121,3 @@ app.post('/api/v1/tours', (req, res) => {
     }
   ); //save this tour to the file
 });
-// WE need to define a route like we done in http before
-// CRUD Operations:
-// POST --> Create
-// GET  --> Read
-// PUT  --> Update
-// PATCH-->
-// DELETE --> DELETE
-
-// Handling DELETEING  requests.
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'faild',
-        message: 'Invalid ID',
-      });
-    }
-  
-    res.status(204).json({ // 204 --> There's no data, the data is deleted and return null to the client
-      status: 'success',
-      data: null
-    });
-      
-  }); // We need to update the data
-  
